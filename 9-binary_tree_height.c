9f6b34b52aa6d8a22bbec17fca409433373e64dc
Adamzou-lab <adampro@icloud.com>
2026-04-07 16:36:49 +0200

    feat: add binary_tree_height and binary_tree_leaves + proto

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height, right_height;
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+    	return (0);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+	if (left_height > right_height)
+		return (1 + left_height);
+	return (1 + right_height);
+}

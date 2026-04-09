d9b6284830eced8819cfa48b904513b0f04b09a7
Adamzou-lab <adampro@icloud.com>
2026-04-09 09:53:20 +0200

    fix: changing the code because recursive is not accepted

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,4 +1,26 @@
 #include "binary_trees.h"
+
+/**
+ * tree_height - Measures the height of a binary tree.
+ * @tree: A pointer to the root node.
+ *
+ * Return: The height of the tree.
+ */
+static size_t tree_height(const binary_tree_t *tree)
+{
+	size_t left_height, right_height;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+	left_height = tree_height(tree->left);
+	right_height = tree_height(tree->right);
+	if (left_height > right_height)
+		return (1 + left_height);
+	return (1 + right_height);
+}
+
 /**
  * binary_tree_balance - Measures the balance factor of a binary tree.
  * @tree: A pointer to the root node of the tree to measure the balance factor.
@@ -14,11 +36,10 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (tree->left == NULL)
 		left_height = -1;
 	else
-		left_height = binary_tree_height(tree->left);
-
+		left_height = tree_height(tree->left);
 	if (tree->right == NULL)
 		right_height = -1;
 	else
-		right_height = binary_tree_height(tree->right);
+		right_height = tree_height(tree->right);
 	return (left_height - right_height);
 }
